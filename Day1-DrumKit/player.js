530238c1a7d9f4a0d5b29a6f28b51608c30e5793
TorrieL <hmminicity2@163.com>
2019-12-07 00:46:38 +0800

    done

--- Day1-DrumKit/player.js
+++ Day1-DrumKit/player.js
@@ -1,27 +1,26 @@
 (function(){
-  window.addEventListener('keydown',playHandler)
-
   function playHandler(e){
     //play music
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     if(audio){
+      //console.log(audio.currentTime)
       audio.currentTime = 0;//cosecutive playing 
       audio.play()
     }
-    
-
     //dom style
     const dom = document.querySelector(`div[data-key="${e.keyCode}"]`)
     if(dom)dom.classList.add('playing')
-
     document.querySelectorAll('.key').forEach( key => {
       key.addEventListener('transitionend',transitionendHandler)
     })
-    
-    function transitionendHandler(e){
+  }
+  
+  function transitionendHandler(e){
       if(e.propertyName === 'transform'){
         e.currentTarget.classList.remove('playing')
       }
     }
-  }
+  
+  window.addEventListener('keydown',playHandler)
+  
 })();

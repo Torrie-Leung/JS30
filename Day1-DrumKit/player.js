c14137663df1929ebc78421be5e8531aa8327006
TorrieL <hmminicity2@163.com>
2019-12-06 23:30:57 +0800

    add eventlistener

--- Day1-DrumKit/player.js
+++ Day1-DrumKit/player.js
@@ -2,6 +2,11 @@
   window.addEventListener('keydown',playHandler)
 
   function playHandler(e){
-    console.log(e)
+    //play music
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+
+    //dom style
+    const dom = document.querySelector(`div[data-key="${e.keyCode}"]`)
+    audio.play()
   }
 })();

e6a90d3cfc4b8160765c6fc8a8fcc75bb22deda7
TorrieL <hmminicity2@163.com>
2019-12-07 00:20:25 +0800

    watching transitionend and rmv playing effect

--- Day1-DrumKit/player.js
+++ Day1-DrumKit/player.js
@@ -4,9 +4,24 @@
   function playHandler(e){
     //play music
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    if(audio){
+      audio.currentTime = 0;//cosecutive playing 
+      audio.play()
+    }
+    
 
     //dom style
     const dom = document.querySelector(`div[data-key="${e.keyCode}"]`)
-    audio.play()
+    if(dom)dom.classList.add('playing')
+
+    document.querySelectorAll('.key').forEach( key => {
+      key.addEventListener('transitionend',transitionendHandler)
+    })
+    
+    function transitionendHandler(e){
+      if(e.propertyName === 'transform'){
+        e.currentTarget.classList.remove('playing')
+      }
+    }
   }
 })();

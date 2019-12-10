139c1854baaaa1299d13169ac710aa461ef7b988
TorrieL <hmminicity2@163.com>
2019-12-10 23:12:03 +0800

    clock initial setup

---
+++ Day2-Clock/clock.js
@@ -0,0 +1,9 @@
+;(function(){
+  const sec = document.querySelector('.second-hand')
+  const min = document.querySelector('.min-hand')
+  const hour = document.querySelector('.hour-hand')
+
+  function setClock(){
+    const data = new Date()
+  }
+})()

6e8d21dc24deb88adbd86388fa46cd31349a6c29
TorrieL <hmminicity2@163.com>
2019-12-11 00:17:52 +0800

    setClock()

--- Day2-Clock/clock.js
+++ Day2-Clock/clock.js
@@ -4,6 +4,16 @@
   const hour = document.querySelector('.hour-hand')
 
   function setClock(){
-    const data = new Date()
+    let data = new Date()
+
+    let secDeg = data.getSeconds() * 6// 360/60
+    let minDeg = data.getMinutes() * 6// 360/60
+    let hourDeg = data.getHours() * 30// 360/12
+
+    sec.style.transform = `rotate(${secDeg}deg)`
+    min.style.transform = `rotate(${minDeg}deg)`
+    hour.style.transform = `rotate(${hourDeg}deg)`
   }
+
+  setClock()//initialize
 })()

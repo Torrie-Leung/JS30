77baef074f94ef694c16c5b2656ba1a575666c51
TorrieL <hmminicity2@163.com>
2019-12-21 21:28:10 +0800

    switch cases

--- Day3-cssVariables/app.js
+++ Day3-cssVariables/app.js
@@ -2,9 +2,22 @@
   const inputs = document.querySelectorAll('.controls input')
 
   function changeHandler(){
-    console.log(this.name)
+    // console.log(this.name,this.value)
+    switch(this.name){
+      case 'spacing':
+        document.querySelector('img').style.padding = this.value + 'px'
+        break;
+      case 'blur':
+        console.log(this.value)
+        document.querySelector('img').style.filter = `blur(${this.value}px)`
+        break;
+      case 'base':
+        document.querySelector('img').style.background = this.value
+        break;
+    }
   }
   inputs.forEach(function(input){
     input.addEventListener('change',changeHandler)
+    input.addEventListener('mousemove',changeHandler)
   })
 })()

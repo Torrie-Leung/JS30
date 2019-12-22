3afba8acf2e2d7b45483221f35562753b08483a4
TorrieL <hmminicity2@163.com>
2019-12-22 22:01:06 +0800

    setProperty&dataset

--- Day3-cssVariables/app.js
+++ Day3-cssVariables/app.js
@@ -3,18 +3,21 @@
 
   function changeHandler(){
     // console.log(this.name,this.value)
-    switch(this.name){
-      case 'spacing':
-        document.querySelector('img').style.padding = this.value + 'px'
-        break;
-      case 'blur':
-        console.log(this.value)
-        document.querySelector('img').style.filter = `blur(${this.value}px)`
-        break;
-      case 'base':
-        document.querySelector('img').style.background = this.value
-        break;
-    }
+    // switch(this.name){
+    //   case 'spacing':
+    //     document.querySelector('img').style.padding = this.value + 'px'
+    //     break;
+    //   case 'blur':
+    //     console.log(this.value)
+    //     document.querySelector('img').style.filter = `blur(${this.value}px)`
+    //     break;
+    //   case 'base':
+    //     document.querySelector('img').style.background = this.value
+    //     document.querySelector('.hl').style.color = this.value
+    //     break;
+    // }
+
+    document.documentElement.style.setProperty('--'+this.name,this.value+(this.dataset.sizing || ''))
   }
   inputs.forEach(function(input){
     input.addEventListener('change',changeHandler)

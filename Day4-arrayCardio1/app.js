e8eef233a27c16f4ee486d3d623d8b4c9f6a706f
TorrieL <hmminicity2@163.com>
2019-12-23 00:23:21 +0800

    filter

--- Day4-arrayCardio1/app.js
+++ Day4-arrayCardio1/app.js
@@ -25,7 +25,14 @@
 
     // Array.prototype.filter()
     // 1. Filter the list of inventors for those who were born in the 1500's
-
+    let answer = inventors.filter(function(inventor){
+      return inventor.year >= 1500 && inventor.year < 1600
+    })
+    console.table(answer)
+    //es6ver 
+    let answ = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600
+    )
+    console.table(answ)
     // Array.prototype.map()
     // 2. Give us an array of the inventors' first and last names
 
